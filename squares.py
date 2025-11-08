e432009f1b75cf170db7ee79c049d2a0e01b3cda
yihengcai0006 <ucapyca@ucl.ac.uk>
2025-11-08 23:39:51 +0000

    Clean up and correct doctests

--- squares.py
+++ squares.py
@@ -1,22 +1,20 @@
 """Computation of weighted average of squares."""
 
-
 def average_of_squares(list_of_numbers, list_of_weights=None):
-    """ Return the weighted average of a list of values.
-    
+    """Return the weighted average of the squares of a list of values.
+
     By default, all values are equally weighted, but this can be changed
     by the list_of_weights argument.
-    
+
     Example:
     --------
     >>> average_of_squares([1, 2, 4])
     7.0
     >>> average_of_squares([2, 4], [1, 0.5])
-    6.0
+    8.0
     >>> average_of_squares([1, 2, 4], [1, 0.5])
     Traceback (most recent call last):
     AssertionError: weights and numbers must have same length
-
     """
     if list_of_weights is not None:
         assert len(list_of_weights) == len(list_of_numbers), \
@@ -24,22 +22,23 @@ def average_of_squares(list_of_numbers, list_of_weights=None):
         effective_weights = list_of_weights
     else:
         effective_weights = [1] * len(list_of_numbers)
+
     squares = [
         weight * number * number
         for number, weight
         in zip(list_of_numbers, effective_weights)
     ]
-    return sum(squares)
+    
+    return sum(squares) / sum(effective_weights)
 
 
 def convert_numbers(list_of_strings):
     """Convert a list of strings into numbers, ignoring whitespace.
-    
+
     Example:
     --------
     >>> convert_numbers(["4", " 8 ", "15 16", " 23    42 "])
-    [4, 8, 15, 16]
-
+    [4.0, 8.0, 15.0, 16.0, 23.0, 42.0]
     """
     all_numbers = []
     for s in list_of_strings:
@@ -51,12 +50,18 @@ def convert_numbers(list_of_strings):
 
 
 if __name__ == "__main__":
-    numbers_strings = ["1","2","4"]
-    weight_strings = ["1","1","1"]        
-    
+    numbers_strings = ["1", "2", "4"]
+    weight_strings = ["1", "1", "1"]
+
     numbers = convert_numbers(numbers_strings)
     weights = convert_numbers(weight_strings)
-    
+
     result = average_of_squares(numbers, weights)
-    
-    print(result)
+    print(f"Weighted average of squares: {result}")
+
+
+
+
+
+
+
